0cdb75bee0be7f370f3b498d95768b8c60ee45ed
frietvorkje69 <justlukeeee@gmail.com>
2023-03-09 15:17:10 +0100

    updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,16 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $posts = Post::latest()->paginate(5);
+
+        return view('posts.index',compact('posts'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+//        $this->authenticated();
+    }
+
+    protected function authenticated() {
+        if (Auth::check()) {
+            return redirect()->route('PostController');
+        }
     }
 }
